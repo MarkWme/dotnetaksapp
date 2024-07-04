2b20b0d17dcd589bca16ad0d7200564a6ee743ef
Mark Whitby <m@markw.me>
2024-07-04 21:46:24 +0100

    Added city to weather forecast endpoint

--- Program.cs
+++ Program.cs
@@ -36,6 +36,21 @@ app.MapGet("/weatherforecast", () =>
 .WithName("GetWeatherForecast")
 .WithOpenApi();
 
+app.MapGet("/weatherforecast/{city}", (string city) =>
+{
+    var forecast = Enumerable.Range(1, 5).Select(index =>
+        new WeatherForecast
+        (
+            DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
+            Random.Shared.Next(-20, 55),
+            summaries[Random.Shared.Next(summaries.Length)]
+        ))
+        .ToArray();
+    return Results.Ok(new { City = city, Forecast = forecast });
+})
+.WithName("GetWeatherForecastByCity")
+.WithOpenApi();
+
 app.MapGet("/", () => Results.Ok(new
 {
     message = "Welcome to the Weather Forecast API. Use the following endpoints to interact with the API:",
